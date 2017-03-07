241152e455ebd81d29ed99ce3829112144260fe1
Tang <tang.biao@gmail.com>
2017-03-07 16:54:46 +0800

    try testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow} from 'enzyme'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders shallow', () => {
+  shallow(<App />);
+});
+
+it('access local storage', () => {
+  localStorage.getItem('key');
+})
